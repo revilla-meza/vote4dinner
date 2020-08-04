86df40144856211123a544c24d808beb9be0e41c
Luis Revilla <luisrevillameza@gmail.com>
2020-08-04 15:43:10 -0400

    Setup Tailwind

--- ui/src/Recipes.tsx
+++ ui/src/Recipes.tsx
@@ -7,26 +7,42 @@ const filterByNamePredicate = (title: string, filter: string) =>
 
 function Recipes() {
   const { state, dispatch, refetch } = useRecipeData();
-  const { recipesById: recipes, recipeIds: ids, nextToken, filteringToken, filter } = state;
-  const token = filter.length > 0 ? filteringToken : nextToken; 
+  const {
+    recipesById: recipes,
+    recipeIds: ids,
+    nextToken,
+    filteringToken,
+    filter,
+  } = state;
+  const token = filter.length > 0 ? filteringToken : nextToken;
 
-  const [timeOutId, setTimeOutId]:[null|number, any] = React.useState(null);
-  const itemsToRender = ids ? ids
-  .filter((id: string) => filterByNamePredicate(recipes[id].title, filter)) : [];
+  const [timeOutId, setTimeOutId]: [null | number, any] = React.useState(null);
+  const itemsToRender = ids
+    ? ids.filter((id: string) =>
+        filterByNamePredicate(recipes[id].title, filter)
+      )
+    : [];
 
-  const renderItems = itemsToRender.map((id: string) => <div key={id}>{recipes[id].title}</div>)
-  
+  const renderItems = itemsToRender.map((id: string) => (
+    <div
+      className="px-4 py-2 m-1 bg-indigo-200 rounded shadow-md"
+      key={id}
+    >
+      <p className="text-xl ml-5">{recipes[id].title}</p>
+    </div>
+  ));
+
   // "filter server-side"
-  React.useEffect(()=>{
-    if(state.filter.length > 0 && itemsToRender.length < 7){
-      if(timeOutId){
+  React.useEffect(() => {
+    if (state.filter.length > 0 && itemsToRender.length < 7) {
+      if (timeOutId) {
         clearTimeout(timeOutId);
       }
-      const id = window.setTimeout(()=>{
-       refetch({nextToken: state.filteringToken, filter: state.filter });
+      const id = window.setTimeout(() => {
+        refetch({ nextToken: state.filteringToken, filter: state.filter });
       }, 500);
       setTimeOutId(id);
-   }
+    }
   }, [state.filter, itemsToRender.length]);
 
   return (
@@ -42,7 +58,9 @@ function Recipes() {
         value={filter}
         placeholder="filter recipes"
       />
+      <div className="grid grid-cols-1">
       {renderItems}
+      </div>
       {token && (
         <button
           onClick={() => {
@@ -57,3 +75,12 @@ function Recipes() {
 }
 
 export default Recipes;
+/**
+ *   
+ * 
+ *    <div className="p-3 m-2 flex flex-row items-center rounded justify-between shadow-sm">
+      
+        <p className={`text-xl ml-5`}>{item.label}</p>
+      
+    </div>
+ */
